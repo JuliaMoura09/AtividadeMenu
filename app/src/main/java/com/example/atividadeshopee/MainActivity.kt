fd91ed11b6811ab73c55fc25163288b8ea7f1d19
20241tiimi0356 <julia.moura.gemea1@gmail.com>
2026-09-17 09:17:40 -0300

    ss

---
+++ app/src/main/java/com/example/atividadeshopee/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.atividadeshopee
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.navigation.NavigationView
+
